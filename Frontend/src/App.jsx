e02cebf1aeeb1b6cd88cd88356f092cea8e463ed
hameed basha <davudroshini00@gmail.com>
2024-05-09 11:35:35 +0530

    Frontend Done!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './Home/Home';
+import { Route, Routes } from "react-router-dom";
+import Courses from './Course/Courses';
+import Signup from './components/Signup';
+import Contacts from './Contact/Contacts'; 
+const App = () => {
+  return (
+  <>
+  <div className="dark:bg-slate-900 dark:text-white">
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/course" element={<Courses />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/contact" element={<Contacts />} />
+   </Routes>
+   </div>
+  </>
+    
+  );
+}
+
+export default App
