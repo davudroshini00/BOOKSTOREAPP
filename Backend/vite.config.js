86fb5fb7e6eb8e74ce6a53d023c4d2c5e70de8f7
davudroshini00 <davudroshini00@gmail.com>
2024-07-20 19:32:41 +0530

    done

--- Backend/vite.config.js
+++ Backend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'backend/frontend', // Adjust the root to point to the frontend directory
+  root: 'frontend', // Adjust the root to point to the frontend directory
   base: './',
   plugins: [react()],
   logLevel: 'info',
